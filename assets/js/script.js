e87c9049ade020956ea3f8fdab66a863f40ee8d0
Zachary Barnes <zachhsavage@gmail.com>
2023-03-19 18:54:57 -0400

    Updated the scheduler to work, but colors do not

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,64 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function () {
+  $(".time-block").each(function () {
+    var timeBlock = $(this).attr("id").split("HH")[1];
+    var currentHour = moment().hour();
+
+    if (timeBlock < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    }
+    else if (timeBlock === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    }
+    else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  })
+
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  $(".saveBtn").on("click", function () {
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+
+      localStorage.setItem(time, text);
   });
   
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+});
+
+
+/*
+GIVEN I am using a daily planner to create a schedule
+
+✓✓✓✓✓  WHEN I open the planner 
+✓✓✓✓✓  THEN the current day is displayed at the top of the calendar
+
+✓✓✓✓✓  WHEN I scroll down
+✓✓✓✓✓  THEN I am presented with time blocks for standard business hours
+
+✓✓✓✓✓  WHEN I view the time blocks for that day
+✓✓✓✓✓  THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+✓✓✓✓✓  WHEN I click into a time block
+✓✓✓✓✓  THEN I can enter an event
+
+✓✓✓✓✓  WHEN I click the save button for that time block
+✓✓✓✓✓  THEN the text for that event is saved in local storage
+
+WHEN I refresh the page
+THEN the saved events persist
+*/
